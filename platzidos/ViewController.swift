7bccc81e293dae14fc39c74050b74e6888d5255b
Alberto Martinez <albon_marvel@hotmail.com>
2017-01-11 18:56:21 -0600

    Agregando datos a la tabla

--- platzidos/ViewController.swift
+++ platzidos/ViewController.swift
@@ -12,18 +12,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var itemTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
-    
+    let todoList = TodoList()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = todoList
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+
     }
 
     @IBAction func addButtonPressed(_ sender: Any) {
         print("hi")
+        todoList.addItem(item: itemTextField.text!)
+        tableView.reloadData()
     }
 }
